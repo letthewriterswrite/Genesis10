f5392ff146da96060ec2760c1022fa2d9405a540
Aaron Munoz <letthewriterswrite@gmail.com>
2018-09-28 18:21:27 -0500

    Created Lucky Sevens game.

---
+++ luckysevens/js/luckysevens.js
@@ -0,0 +1,102 @@
+//Main Game controller
+function runGame()
+{
+    var dollars = document.forms["luckySevens"]["dollars"].value;
+    
+    //Validate input
+   if(dollars == "" || isNaN(dollars)){
+       alert("Must enter a dollar amount.");
+       
+       document.forms["luckySevens"]["dollars"].focus();
+         
+       return false;
+   }
+    
+    //Start game
+    playGame(dollars);
+    
+    //Give player ability to play again
+    document.getElementById("resultsTable").className = "table table-bordered ";
+    document.getElementById("resultsHeading").className = "text-center";
+    document.getElementById("playButton").type = "reset";
+    document.getElementById("playButton").value = "Play Again";
+
+    return false;
+}
+
+//Dice Generator
+function rollDice ()
+{
+    
+    return Math.floor(Math.random()*6)+1;
+
+}
+
+//Game Round
+function playGame(dollars) {
+//Initialize variables to track data
+    var startingBet = dollars;
+    var totalRolls = 1;
+    var maxMoney = dollars;
+    var maxRolls = totalRolls;
+//Play game until hitting zero
+    for (var dice1,dice2,i=1;dollars > 0;i++)
+        {
+            dice1 = rollDice();
+            dice2 = rollDice();
+            //Log dice rolls and roll number
+            console.log("dice " + (dice1 + dice2));
+            console.log("roll # " + i)
+
+            if(dice1+dice2 == 7){
+                dollars += 4;
+            }else
+                {
+                    dollars -= 1;
+                }
+
+            //Tracks the most money made and at what number of rolls
+            if(dollars > maxMoney)
+                {
+                    maxMoney = dollars;
+                    maxRolls = totalRolls;
+                }
+            totalRolls++;
+            //Log dollars left
+            console.log(dollars);
+        }
+    //Call function to display results
+    displayResults(startingBet,totalRolls,maxMoney,maxRolls);
+}
+
+
+//Function to display results
+function displayResults (startingBet,totalRolls,maxMoney,maxRolls)
+{
+    console.log("starting bet " + startingBet )
+    document.getElementById("startingBet").innerHTML = startingBet;
+    
+    console.log("total rolls " + totalRolls );
+    document.getElementById("totalRolls").innerHTML = totalRolls;
+    
+    console.log("max money " + maxMoney);
+    document.getElementById("maxMoney").innerHTML = maxMoney;
+    
+    console.log("max rolls " + maxRolls);
+    document.getElementById("maxRolls").innerHTML = maxRolls;
+    
+
+    
+}
+
+//Resets Game
+function resetGame()
+{
+    
+    document.getElementById("resultsTable").className = "d-none";
+    document.getElementById("resultsHeading").className = "d-none";
+    document.getElementsByName("dollasr").value = "";
+    document.getElementById("playButton").value = "Play";
+    document.getElementById("playButton").type = "submit";
+    
+}
